944a35f1933b6d6f00a077917f8ac62da73d87d7
mikhail99 <mihail_makarov@yahoo.com>
2013-07-24 14:51:54 +0000

    web.js read index.html

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ 
+
+   fs.readFileSync('index.html',function (err, data){
+     
+        responce.send(data);
+    });
+
 });
 
 var port = process.env.PORT || 5000;
